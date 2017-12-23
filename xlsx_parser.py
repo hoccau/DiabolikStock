6b5f9b4c6a4ee803abe9222b4029d4024ecfbfdc
hoccau <hoccau@kidivid.org>
2017-12-23 17:37:06 +0100

    adapt xlsx parser (work in progress)

--- xlsx_parser.py
+++ xlsx_parser.py
@@ -7,40 +7,54 @@ Parse an xlsx (Excel) file to populate the database with malles
 
 from openpyxl import load_workbook
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+from PyQt5.QtCore import QSettings
+from db import Query
 import re
 
-wb = load_workbook(filename = "liste_malles_2017.xlsx", read_only=True)
+wb = load_workbook(filename = "liste_malles_2018.xlsx", read_only=True)
 ws = wb.active
 
 ws.max_row = 525
-ws.max_column = 6 # means 'F' column
+ws.max_column = 7 # means 'G' column
 print(ws.calculate_dimension()) # is it good ?
-
-database = QSqlDatabase.addDatabase('QPSQL')
-database.setHostName("localhost")
-database.setDatabaseName("testdiabostock")
-database.setUserName("hoccau")
-database.setPassword('password')
-if database.open():
-    print('database connexion ok!')
-
-# Because type definition is not clear, I define a SansType type. 
-QSqlQuery("INSERT INTO malles_types(denomination, observation) VALUES ('SansType', 'Ce type doit rester sans produit. Il sert simplement à définir des malles sans aucun type')")
-QSqlQuery("INSERT INTO lieux(nom, ville, cp, numero, rue) VALUES \
-('Base logistique Ribemont', 'Ribemont-sur-Ancre', 80800, 0, '')")
 
 def get_id(query):
     req = QSqlQuery(query)
     while req.next():
         return req.value(0)
 
-type_id = get_id("SELECT id FROM malles_types WHERE denomination = 'SansType'")
-lieu_id = get_id("SELECT id FROM lieux WHERE nom = 'Base logistique Ribemont'")
-
 regex = '^[A-Z]-[A-Z][0-9]{2}$' # regex for location (Adresse col)
 
+def get_all_types(sheet):
+    types = []
+    for row in sheet.iter_rows(row_offset=1):
+        type_, number, nom, sousclasse, classe, adresse, observation = row
+        if sousclasse not in types:
+            types.append(sousclasse)
+    return types
+
+def get_all_categories(sheet):
+    categories = []
+    for row in sheet.iter_rows(row_offset=1):
+        type_, number, nom, sousclasse, classe, adresse, observation = row
+        if classe not in types:
+            types.append(sousclasse)
+    return types
+
+def get_col_as_list(col, sheet):
+    res = []
+    for row in sheet.iter_rows(row_offset=1):
+        if row[col] not in res:
+            res.append(row[col])
+    return res
+
+malles_types = get_col_as_list(3, ws)
+categories = get_col_as_list(4, ws)
+print(malles_types)
+print(categories)
+
 def populate_db(xlsx_row):
-    type_, number, nom, classe, adresse, observation = xlsx_row
+    type_, number, nom, sousclasse, classe, adresse, observation = xlsx_row
     if not type_.value or not number.value:
         return False
     reference = type_.value + str(number.value)
@@ -55,9 +69,10 @@ def populate_db(xlsx_row):
         section, slot, shelf = '', '', ''
     query = QSqlQuery()
     query.prepare("INSERT INTO malles(\
-    reference, type_id, lieu_id, section, shelf, slot, observation) \
-    VALUES(?, ?, ?, ?, ?, ?, ?)")
+    reference, category_id, type_id, lieu_id, section, shelf, slot, observation) \
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?)")
     query.addBindValue(reference)
+    query.addBindValue(category_id)
     query.addBindValue(type_id)
     query.addBindValue(lieu_id)
     query.addBindValue(section)
@@ -65,6 +80,18 @@ def populate_db(xlsx_row):
     query.addBindValue(slot)
     query.addBindValue(observation.value)
     query.exec_()
+
+db = Query()
+settings = QSettings('Kidivid', 'DiabolikStock')
+connected = db.connect(settings)
+
+if connected:
+    print('database connexion ok!')
+
+# Location by default
+QSqlQuery("INSERT INTO lieux(nom, ville, cp, numero, rue) VALUES \
+('Base logistique Ribemont', 'Ribemont-sur-Ancre', 80800, 0, '')")
+lieu_id = get_id("SELECT id FROM lieux WHERE nom = 'Base logistique Ribemont'")
 
 for row in ws.iter_rows(row_offset=1):
     populate_db(row)
